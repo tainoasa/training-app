8403f6a3bae35117f3d864257e58cfdee4a50574
yusuke20000220 <kobayashi.yusuke.infratop@gmail.com>
2023-10-05 22:09:42 +0900

    yusuke2

--- backend/web-back/todo/urls.py
+++ backend/web-back/todo/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from .views import ListTodo, DetailTodo, ListTraining_history, DetailTraining_history,ListTraining_list, DetailTraining_list
 from .views import CreateTrainingHistoryView, CreateRecommendHistoryView
-from .views import DummyRespponseApiView
+from .views import DummyRespponseApiView,ChatRespponseApiView
 
 urlpatterns = [
     path('todo/<int:pk>/', DetailTodo.as_view()),
@@ -12,5 +12,6 @@ urlpatterns = [
     path('training_list/', ListTraining_list.as_view()),
     path('training_history/create/',CreateTrainingHistoryView.as_view()),
     path('recommend_history/create/',CreateRecommendHistoryView.as_view()),
-    path('response/',DummyRespponseApiView.as_view()),
+    path('chat_dummy/',DummyRespponseApiView.as_view()),
+    path('chat/',ChatRespponseApiView.as_view()),
 ]
--- backend/web-back/todo/views.py
+++ backend/web-back/todo/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework.response import Response
+import requests
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 from rest_framework import generics, viewsets
 from .models import Todo, Training_history, Training_list, Recommend_history
@@ -45,8 +46,16 @@ class CreateRecommendHistoryView(generics.CreateAPIView):
 class DummyRespponseApiView(generics.ListAPIView):
     authentication_classes = (CsrfExemptSessionAuthentication, BasicAuthentication)
     def post(self, request, format=None):
+        res = requests.post('http://172.20.7.60:2150/chat',json={"text":"野球するには何の筋トレすればいいですか？"})
         json = {"name": "DUMMY!","menu":["ベンチプレス","ペックフライ"]}
         return Response(json)
+
+    
+class ChatRespponseApiView(generics.ListAPIView):
+    authentication_classes = (CsrfExemptSessionAuthentication, BasicAuthentication)
+    def post(self, request, format=None):
+        res = requests.post('http://172.20.7.60:2150/chat',json={"text":"hello"})
+        return Response(res)
 
 
 
